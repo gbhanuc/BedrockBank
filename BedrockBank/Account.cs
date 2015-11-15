f884969ae67e206a8e51c06787f39aa4f5328a8e
gbhanuc <gbhanuc@live.com>
2015-11-15 11:06:47 -0800

    Initial Code

---
+++ BedrockBank/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    /// <summary>
+    /// This class provides definition for a Bedrock Bank
+    /// </summary>
+    class Account
+    {
+        #region properties
+        /// <summary>
+        /// Name of the account holder
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Account number  for the account
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// Account Balance for the account
+        /// </summary>
+        public decimal Balance { get; set; }                                                                                                                                                                          
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
